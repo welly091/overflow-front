0881dd000a275b5f4f57a80499b37a52be6a9131
Alec Torres <atorresla24@gmail.com>
2022-09-21 14:12:11 -0400

    gave some styling to footer, header and login page

--- components/Header.js
+++ components/Header.js
@@ -6,24 +6,24 @@ function Header() {
   const { user, logout } = useAuth()
 
   return (
-    <header className='flex justify-between items-center bg-green-400 p-3.5'>
-      <h1 className='text-4xl'>Code Overflow</h1>
+    <header className='flex justify-between items-center bg-black p-3.5 font-serif'>
+      <h1 className='text-4xl font-bold text-white'>Code Overflow</h1>
       <div>
         <nav>
           <Link href={"/"}>
-            <a className="inline-block w-20 text-white bg-green-600 p-0.5 mx-0.5 rounded border border-black">Home</a>
+            <a className="inline-block w-20 text-white bg-red-600 p-0.5 mx-0.5 rounded-lg border border-red-500">Home</a>
           </Link>
           <Link href={'/about-us'} >
-            <a className="inline-block w-20 bg-gray-100 p-0.5 mx-0.5 rounded border border-black text-center">About Us</a>
+            <a className="inline-block w-20 bg-gray-100 p-0.5 mx-0.5 rounded-lg border border-black text-center">About Us</a>
           </Link>
           {
-            user && <p className="inline-block w-20 bg-gray-100 p-0.5 mx-0.5 rounded border border-black text-center">{user.username}</p>
+            user && <p className="inline-block w-20 bg-gray-100 p-0.5 mx-0.5 rounded-lg border border-black text-center">{user.username}</p>
           }
-          <button onClick={logout} className="inline-block w-20 text-white bg-green-600 p-0.5 mx-0.5 rounded border border-black">Sign Up</button>
+          <button onClick={logout} className="inline-block w-20 text-white bg-red-600 p-0.5 mx-0.5 rounded-lg border border-red-500">Sign Up</button>
           {user ?
-            <p onClick={logout} className="inline-block w-20 bg-gray-100 p-0.5 mx-0.5 rounded border border-black text-center">Logout</p> :
+            <p onClick={logout} className="inline-block w-20 bg-gray-100 p-0.5 mx-0.5 rounded-lg border border-black text-center">Logout</p> :
             <Link href={'/login'} >
-              <a className="inline-block w-20 bg-gray-100 p-0.5 mx-0.5 rounded border border-black text-center">Sign in</a>
+              <a className="inline-block w-20 bg-gray-100 p-0.5 mx-0.5 rounded-lg border border-black text-center">Sign in</a>
             </Link>}
         </nav>
       </div>
--- pages/login.js
+++ pages/login.js
@@ -15,11 +15,11 @@ export default function Login(props) {
 
   return (
     <>
-      <form onSubmit={handleSubmit} className='flex flex-col w-5/6 p-4 m-auto my-5 space-y-5 text-center border-2 border-black rounded-md'>
-        <label className='flex flex-row mb-4 text-4xl font-bold'> Username
+      <form onSubmit={handleSubmit} className='flex flex-col w-3/5 p-3 pb-24 m-auto my-5 space-y-5 text-center border-2 border-black rounded-md mt-28'>
+        <label className='flex flex-row mb-4 ml-8 text-4xl font-bold'> Username
           <input name='username' type='text' placeholder='username or email' className='mx-4 bg-gray-100' />
         </label>
-        <label className='flex flex-row mb-4 text-4xl font-bold'> Password
+        <label className='flex flex-row mb-4 ml-10 text-4xl font-bold'> Password
           <input name='password' type='password' placeHolder='password' className='mx-4 bg-gray-100' />
         </label>
         <button type='submit' className='w-5/6 bg-blue-200 border-black'>Login</button>
