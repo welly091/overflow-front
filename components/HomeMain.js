b0ea1da8192377d2d9889c9acf5d72e816717896
Falashade Greene <falashadegreene@gmail.com>
2022-09-20 21:20:34 -0400

    added side contents just need to complete styling

--- components/HomeMain.js
+++ components/HomeMain.js
@@ -7,6 +7,43 @@ return  (
     </label>
     <button type='submit' className='ml-4 rounded-lg bg-slate-400 text-white px-8 py-2'>Submit</button>
   </form>
+
+  <div className=''>
+    <nav className="flex-auto py-4 px-6 border-b border-gray-200">
+      <h1>Common Categories</h1>
+      <li href="/">
+        <a>Data Structures</a>
+      </li>
+      <li href="">
+        <a>Javascript</a>
+      </li>
+      <li href=''>
+        <a>Python</a>
+      </li>
+      <li href=''>
+        <a>Career Readiness</a>
+      </li>
+    </nav>
+  </div>
+
+  <div className=''>
+    <nav className="flex-auto py-4 px-6 border-b border-gray-200">
+      <h1>Level</h1>
+      <li href="/">
+        <a>102</a>
+      </li>
+      <li href="">
+        <a>201</a>
+      </li>
+      <li href=''>
+        <a>301</a>
+      </li>
+      <li href=''>
+        <a>401</a>
+      </li>
+    </nav>
+  </div>
+  
   <div className='flex-auto ml-96 pr-96 w-full box-border h-32 w-32 p-4 border-4 ... mt-16'><p className='text-slate-400'>Question</p></div>
   <div className='flex-auto ml-96 pr-96 w-full box-border h-32 w-32 p-4 border-4 ... mt-16 mt-3'><p className='text-slate-400'>Question</p></div>
   <div className='flex-auto ml-96 pr-96 w-full box-border h-32 w-32 p-4 border-4 ... mt-16 mt-3'><p className='text-slate-400'>Question</p></div>
