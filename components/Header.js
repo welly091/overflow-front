4a02026b4b9d2ac8a3ce272e3e52cf9d9f998cba
Alec Torres <atorresla24@gmail.com>
2022-09-21 16:59:58 -0400

    sign up page and tailwind css in sign up, login, header

--- components/Header.js
+++ components/Header.js
@@ -11,19 +11,19 @@ function Header() {
       <div>
         <nav>
           <Link href={"/"}>
-            <a className="inline-block w-20 text-white bg-red-600 p-0.5 mx-0.5 rounded-lg border border-red-500">Home</a>
+            <a className="inline-block w-20 text-white bg-red-600 p-0.5 mx-0.5 rounded-lg border border-red-500 transition ease-in-out delay-150 hover:-translate-y-1 hover:scale-110 hover:bg-red-500 duration-300 ...">Home</a>
           </Link>
           <Link href={'/about-us'} >
-            <a className="inline-block w-20 bg-gray-100 p-0.5 mx-0.5 rounded-lg border border-black text-center">About Us</a>
+            <a className="inline-block w-20 bg-gray-100 p-0.5 mx-0.5 rounded-lg border border-black text-center transition ease-in-out delay-150 hover:-translate-y-1 hover:scale-110 hover:bg-gray-100 duration-300 ...">About Us</a>
           </Link>
           {
             user && <p className="inline-block w-20 bg-gray-100 p-0.5 mx-0.5 rounded-lg border border-black text-center">{user.username}</p>
           }
-          <button onClick={logout} className="inline-block w-20 text-white bg-red-600 p-0.5 mx-0.5 rounded-lg border border-red-500">Sign Up</button>
+          <button onClick={logout} className="inline-block w-20 text-white bg-red-600 p-0.5 mx-0.5 rounded-lg border border-red-500 transition ease-in-out delay-150 hover:-translate-y-1 hover:scale-110 hover:bg-red-500 duration-300 ...">Sign Up</button>
           {user ?
             <p onClick={logout} className="inline-block w-20 bg-gray-100 p-0.5 mx-0.5 rounded-lg border border-black text-center">Logout</p> :
             <Link href={'/login'} >
-              <a className="inline-block w-20 bg-gray-100 p-0.5 mx-0.5 rounded-lg border border-black text-center">Sign in</a>
+              <a className="inline-block w-20 bg-gray-100 p-0.5 mx-0.5 rounded-lg border border-black text-center transition ease-in-out delay-150 hover:-translate-y-1 hover:scale-110 hover:bg-gray-100 duration-300 ...">Sign in</a>
             </Link>}
         </nav>
       </div>
