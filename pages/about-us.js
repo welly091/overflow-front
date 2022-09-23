e8a68c2e7cd7c8d2b5e3d3bd9b04956666f7d52f
welly091 <whtp1490@gmail.com>
2022-09-23 09:40:32 -0700

    h

--- pages/about-us.js
+++ pages/about-us.js
@@ -1,7 +1,6 @@
 import React, {Component} from 'react';
 import "react-responsive-carousel/lib/styles/carousel.min.css";
 import {Carousel} from 'react-responsive-carousel';
-import Image from 'next/image';
 import {Github, Linkedin} from 'react-bootstrap-icons';
 
 export default class NextJsCarousel extends Component {
